15719b365cd0eaf76303668cbcea7dd3f04cf511
Reneilwe Sesoko <reneilwe.sesoko@dadevs.co.za>
2022-02-11 16:57:30 +0200

    menu and services

---
+++ frontend/src/app/api/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl = 'http://localhost:3000/api/v1/users';
+
+  constructor(private http: HttpClient) { }
+
+  //create a user
+  addUser(user: any): Observable<any>{
+    return this.http.post(this.baseUrl, user);
+  }
+
+  //read user
+  getUsers(): Observable<any>{
+    return this.http.get<any>(this.baseUrl)
+  }
+
+  //read user by id
+  getUser(id: any): Observable<any>{
+    return this.http.get<any>(`${this.baseUrl}/${id}`)
+  }
+
+  //update
+  updateUser(user: any, id: any): Observable<any>{
+    return this.http.put<any>(`${this.baseUrl}/${id}`, user)
+  }
+
+  
+
+
+
+
+}
