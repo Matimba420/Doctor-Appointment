2ed4efa332a77e55162cafaebc8a65c305f13ad7
ntsakokhoza <ntsako.khoza@dadevs.co.za>
2022-02-15 02:31:18 +0200

    ft-updated login and registration for clients/doctors

--- frontend/src/app/login/login.page.ts
+++ frontend/src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
 import { UserService } from '../api/user.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -10,7 +12,7 @@ import { UserService } from '../api/user.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) { }
 
   registerForm = new FormGroup({
     firstname: new FormControl('', [Validators.required]),
@@ -19,17 +21,27 @@ export class LoginPage implements OnInit {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)]),
   })
-
+  
   get form(){
     return this.registerForm.controls;
   }
 
   register(){
-    this.userService.addUser(this.registerForm.value).subscribe((data: any)=>{
-      console.log(data)
-    })
+    // this.userService.addUser(this.registerForm.value).subscribe((data: any)=>{
+    //   console.log(data)
+    //   this.router.navigate(['/login']);
+    // })
+
+    this.userService.addUser(this.registerForm.value).subscribe({
+      next:(data) =>{
+        console.log(data)
+        this.router.navigate(['/login']);
+      }
     
-  }
+    });
+    }
+
+    
 
   ngOnInit() {
   }
