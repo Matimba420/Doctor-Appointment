35c2772fb89dc619ef80d9fbd206bfc46c5f9bd5
Ntsako Khoza <ntsako.khoza@dadevs.co.za>
2022-02-17 16:50:59 +0200

    ft-logout

--- backend/src/queries/queries.js
+++ backend/src/queries/queries.js
@@ -1,7 +1,7 @@
 const addClient = "INSERT INTO PUBLIC.USER (firstname, lastname, cell_no, email,password) VALUES ($1,$2,$3,$4,$5)";
 const getClients = "SELECT * FROM PUBLIC.USER where is_active='true' ORDER BY id ASC";
 const getClientById = "SELECT * FROM PUBLIC.USER WHERE id =$1";
-const checkClientEmailExists = "SELECT * FROM PUBLIC.USER WHERE email= $1";
+const checkClientEmailExists = "SELECT * FROM PUBLIC.USER WHERE email= $1 and is_active='true'";
 const removeClient ="UPDATE PUBLIC.USER SET is_active='false' WHERE id=$1";
 const updateClient ="UPDATE PUBLIC.USER SET cell_no=$1, password=$2 WHERE ID = $3";
 const clientLogin = "SELECT id, firstname, lastname, cell_no, email FROM PUBLIC.USER WHERE password=$1 AND email=$2";
@@ -14,7 +14,7 @@ const activateClient="UPDATE PUBLIC.USER SET is_active=$1 WHERE id=$2 "
 const addDoctor = "INSERT INTO PUBLIC.DOCTOR (dr_name, occupation, experience, company, cell_no, email, password) VALUES ($1,$2,$3,$4,$5,$6,$7)";
 const getDoctors = "SELECT * FROM PUBLIC.DOCTOR  where is_active='true'  ORDER BY id ASC";
 const getDoctorById = "SELECT * FROM PUBLIC.DOCTOR  WHERE id =$1 and is_active='true'"
-const checkDoctorEmailExists = "SELECT * FROM PUBLIC.DOCTOR  WHERE email= $1"
+const checkDoctorEmailExists = "SELECT * FROM PUBLIC.DOCTOR  WHERE email= $1 and is_active='true'"
 const removeDoctor ="UPDATE PUBLIC.DOCTOR  SET is_active='false' WHERE id=$1";
 const updateDoctor ="UPDATE PUBLIC.DOCTOR  SET cell_no=$1, password=$2 WHERE ID = $3"
 const DoctorLogin = "SELECT id, firstname, lastname, cell_no, email FROM PUBLIC.DOCTOR  WHERE password=$1 AND email=$2";
